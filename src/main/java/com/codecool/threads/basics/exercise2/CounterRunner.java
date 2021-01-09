80c4cb119a774eaf7d8a7adaa80aa09feb095377
irrehaare <wjtkwilk@gmail.com>
2021-01-09 16:35:35 +0100

    Exercises 2 & 3

--- src/main/java/com/codecool/threads/basics/exercise2/CounterRunner.java
+++ src/main/java/com/codecool/threads/basics/exercise2/CounterRunner.java
@@ -1,5 +1,7 @@
 package com.codecool.threads.basics.exercise2;
 
+import static java.lang.Thread.sleep;
+
 public class CounterRunner implements Runnable {
     private int counter = 1;
     private static final int MAX = 10000;
@@ -7,10 +9,16 @@ public class CounterRunner implements Runnable {
     @Override
     public void run() {
         System.out.println("Counting started...");
-        while (counter <= MAX) {
-            System.out.println(counter);
-            counter++;
-        }
-        System.out.println("Counting finished.");
+        try {
+                while (counter <= MAX) {
+                    sleep(200);
+                    System.out.println(counter);
+                    counter++;
+                }
+                System.out.println("Counting finished.");
+            } catch (InterruptedException e) {
+                System.out.println("The forces of evil have interrupted me! I still have " + (MAX-counter+1) + " numbers to count!");
+            }
+
     }
 }
--- src/main/java/com/codecool/threads/basics/exercise3/Main.java
+++ src/main/java/com/codecool/threads/basics/exercise3/Main.java
@@ -2,7 +2,10 @@ package com.codecool.threads.basics.exercise3;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new FibonacciGeneratorThread(30);
+        thread.start();
+        thread.join();
         System.out.println("The fibonacci series calculation has been finished!");
     }
 }
