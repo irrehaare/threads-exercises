d8535ea267bfbce84824fa1cdeac8ccbeaf19b3c
irrehaare <wjtkwilk@gmail.com>
2021-01-09 16:35:35 +0100

    Exercises 2 & 3

--- src/main/java/com/codecool/threads/exercise2/Main.java
+++ src/main/java/com/codecool/threads/exercise2/Main.java
@@ -1,8 +1,15 @@
 package com.codecool.threads.exercise2;
 
+import static java.lang.Thread.sleep;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread counter = new Thread(new CounterRunner());
+        counter.start();
 
+        sleep(11111);
+
+        counter.interrupt();
     }
 }
