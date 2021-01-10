edc686bc2df8ad10421bc644b916207b77db11a3
irrehaare <wjtkwilk@gmail.com>
2021-01-10 12:18:28 +0100

    Done Exercise 4

--- src/main/java/com/codecool/threads/basics/exercise4/Main.java
+++ src/main/java/com/codecool/threads/basics/exercise4/Main.java
@@ -2,18 +2,25 @@ package com.codecool.threads.basics.exercise4;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Counter counter = new Counter();
+    public static void main(String[] args) throws InterruptedException {
+        int n=100;
+        while (n>0) {
+            Counter counter = new Counter();
 
-        int limit = 200_000;
+            int limit = 200_000;
 
-        Incrementer incrementer1 = new Incrementer(counter, limit);
-        Incrementer incrementer2 = new Incrementer(counter, limit);
+            Incrementer incrementer1 = new Incrementer(counter, limit);
+            Incrementer incrementer2 = new Incrementer(counter, limit);
 
-        incrementer1.start();
-        incrementer2.start();
+            incrementer1.start();
+            incrementer2.start();
 
-        System.out.printf("Expected: %s | Current: %s%n", limit * 2, counter.getCount());
+            incrementer1.join();
+            incrementer2.join();
+
+            System.out.printf("Expected: %s | Current: %s%n", limit * 2, counter.getCount());
+            n--;
+        }
     }
 
 }
