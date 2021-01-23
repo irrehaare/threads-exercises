d3051132e51bf283b3937734ebbea751da4fde62
irrehaare <wjtkwilk@gmail.com>
2021-01-23 12:44:01 +0100

    Complete task 7

--- src/main/java/com/codecool/threads/advanced/excercises/excercise7/App.java
+++ src/main/java/com/codecool/threads/advanced/excercises/excercise7/App.java
@@ -1,9 +1,24 @@
 package com.codecool.threads.advanced.excercises.excercise7;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        Counter c1 = new Counter(countDownLatch, 5);
+        Counter c2 = new Counter(countDownLatch, 3);
+        Counter c3 = new Counter(countDownLatch, 7);
 
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        executorService.submit(c1);
+        executorService.submit(c2);
+        executorService.submit(c3);
+
+        countDownLatch.await();
+        System.out.println("All your counts are belong to us!");
     }
 
 }
--- src/main/java/com/codecool/threads/advanced/excercises/excercise7/Counter.java
+++ src/main/java/com/codecool/threads/advanced/excercises/excercise7/Counter.java
@@ -1,5 +1,29 @@
 package com.codecool.threads.advanced.excercises.excercise7;
 
-public class Counter extends Thread {
+import java.util.concurrent.CountDownLatch;
 
+public class Counter extends Thread {
+    CountDownLatch finishSignal;
+    int maxValue;
+    int count;
+
+    Counter(CountDownLatch finishSignal, int maxValue) {
+        this.finishSignal = finishSignal;
+        this.maxValue = maxValue;
+        this.count = 0;
+    }
+
+    public void run(){
+        while (count < maxValue) {
+            try {
+                sleep(1000);
+                System.out.println(currentThread() + ": step " + count);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            count++;
+        }
+        System.out.println(currentThread() + " has finished.");
+        finishSignal.countDown();
+    }
 }
